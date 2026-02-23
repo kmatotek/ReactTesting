71628b185e3d513495c2e766e99876a1cb458a0e
kmatotek <km998744@wcupa.edu>
2026-02-22 21:17:23 -0500

    added stuffs

--- app/server/src/controllers/authController.ts
+++ app/server/src/controllers/authController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from 'express';
+import prisma from '../lib/prisma'
 
 //Promise is a built-in object used to handle asynchronous operations.
 // A Promise exists in one of three states: 
 // Pending (in progress), Fulfilled (successful), or Rejected (failed). 
 
+export const register = async (req: Request, res: Response): Promise<void> => {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.create({
+        data: { email, password }
+    });
+
+    res.status(200).json(user);
+}
+
 export const login = async (req: Request, res: Response): Promise<void> => {
   // your login logic here
-  res.json({ message: 'logged in' });
+  res.status(200).json({ message: 'logged in' });
 };
